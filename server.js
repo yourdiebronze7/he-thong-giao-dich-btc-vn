64113594dd119c2b0ad084587b162224d2c551e8
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-07 18:47:10 +0000

    Refactor PORT to use environment variable with fallback

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Middleware for URL-encoded data
