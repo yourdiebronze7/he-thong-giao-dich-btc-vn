44bb9ef598d4e950dd8a2284d84c740d09f489ea
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-07 18:45:45 +0000

    Update server.js to add new middleware for handling URL-encoded data.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Added middleware for URL-encoded data
 
 // Thêm router khác và middleware tại đây
 
